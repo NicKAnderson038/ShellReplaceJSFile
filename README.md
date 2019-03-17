cd9c9e75c2a8f47f1ebda1925175fa0bc470eed4
NicKAnderson038 <andersonnick038@gmail.com>
2019-03-16 22:26:39 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Shell Replication JS File
+
+
+#### Commandline Example
+
+```sh
+bash ./scripts/run.sh
+```
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import _ from 'lodash';
+
+const arg = [1,2,3,4]
+
+const result = _.map(arg, (x)=>x+1)
+
+console.log(result)
+
+'foooo'
